bd64b49cba16f4df61401d3b482081313199b707
Vitek <vitek@meloun.info>
2025-09-14 22:03:57 +0200

    WIP

--- lib/gotsha.rb
+++ lib/gotsha.rb
@@ -5,15 +5,34 @@ require "fileutils"
 require_relative "gotsha/version"
 
 module Gotsha
-  # STFU Rubocop :-)
+  class NoCommandConfigured < StandardError; end
+
+  CONFIG_DIR = ".gotsha"
+  COMMANDS_FILE = "#{CONFIG_DIR}/commands"
+
+  # Main entry
   class CLI
-    def self.call(command) = new.public_send(command)
+    def self.call(action = :run)
+      action ||= :run
+
+      new.public_send(action)
+    end
 
     def init
       puts "Creating default config files..."
-      FileUtils.mkdir_p(".gotsha")
-      FileUtils.touch(".gotsha/commands")
+
+      FileUtils.mkdir_p(CONFIG_DIR)
+      FileUtils.touch(COMMANDS_FILE)
+
       puts "Done"
+    end
+
+    def run
+      raise NoCommandConfigured unless File.exist?(COMMANDS_FILE)
+
+      command = File.read(Gotsha::COMMANDS_FILE)
+
+      # system(command) TODO: causes recursion
     end
   end
 end
--- spec/gotsha_spec.rb
+++ spec/gotsha_spec.rb
@@ -3,12 +3,56 @@
 require "fileutils"
 
 RSpec.describe Gotsha::CLI do
-  describe "#init" do
+  describe "init" do
     it "sets up .gotsha/commands file" do
       expect(FileUtils).to receive(:mkdir_p).with(".gotsha")
       expect(FileUtils).to receive(:touch).with(".gotsha/commands")
 
-      described_class.call("init")
+      described_class.call(:init)
+    end
+  end
+
+  describe "without any action passed" do
+    it "calls `run`" do
+      expect_any_instance_of(described_class).to receive(:run)
+
+      described_class.call
+    end
+  end
+
+  describe "run" do
+    context "without test command configured" do
+      before do
+        expect(File)
+          .to receive(:exist?).with(Gotsha::COMMANDS_FILE)
+          .and_return(false)
+      end
+
+      it "fails with proper error" do
+        expect do
+          described_class.call(:run)
+        end.to raise_exception(Gotsha::NoCommandConfigured)
+      end
+    end
+
+    context "with a test command configured" do
+      let(:test_command) { "bundle exec rspec" }
+
+      before do
+        expect(File)
+          .to receive(:exist?).with(Gotsha::COMMANDS_FILE)
+          .and_return(true)
+
+        expect(File)
+          .to receive(:read).with(Gotsha::COMMANDS_FILE)
+          .and_return(test_command)
+      end
+
+      it "runs the command" do
+        expect(Kernel).to receive(:system).with(test_command)
+
+        described_class.call(:run)
+      end
     end
   end
 end
