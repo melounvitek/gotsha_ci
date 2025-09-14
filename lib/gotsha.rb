c7e83ef2f557722d4b52b5c436c3cb5b4d939665
Vitek <vitek@meloun.info>
2025-09-14 19:59:22 +0200

    Add some verbose output

--- lib/gotsha.rb
+++ lib/gotsha.rb
@@ -6,13 +6,13 @@ require_relative "gotsha/version"
 
 module Gotsha
   class CLI
-    def self.call(command)
-      new.public_send(command)
-    end
+    def self.call(command) = new.public_send(command)
 
     def init
+      puts "Creating default config files..."
       FileUtils.mkdir_p(".gotsha")
       FileUtils.touch(".gotsha/commands")
+      puts "Done"
     end
   end
 end
