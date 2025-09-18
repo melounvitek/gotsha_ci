9ce0d42c2c658677d0d79c07723ae0beba79bae7
Vitek <vitek@meloun.info>
2025-09-18 19:37:59 +0200

    Unify paths

--- lib/gotsha.rb
+++ lib/gotsha.rb
@@ -11,10 +11,10 @@ module Gotsha
 
   CONFIG_DIR = ".gotsha"
   CONFIG_FILE = File.join(CONFIG_DIR, "config.yml")
-  CONFIG_TEMPLATE_PATH = File.expand_path("gotsha/templates/config.yml", __dir__)
+  CONFIG_TEMPLATE_PATH = "lib/gotsha/templates/config.yml"
   GH_CONFIG_FILE = File.join(CONFIG_DIR, "github_action_example.yml")
-  GH_CONFIG_TEMPLATE_PATH = File.expand_path("gotsha/templates/github_action_example.yml", __dir__)
-  HOOKS_TEMPLATES_DIR = File.expand_path("gotsha/templates", __dir__)
+  GH_CONFIG_TEMPLATE_PATH = "lib/gotsha/templates/github_action_example.yml"
+  HOOKS_TEMPLATES_DIR = "lib/gotsha/templates"
   HOOKS_DIR = File.join(CONFIG_DIR, "hooks")
 
   # Main entry
