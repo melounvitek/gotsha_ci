199dfde7df3f204d2c00e1df756ecd8af33f65ee
Vitek <vitek@meloun.info>
2025-09-18 23:56:11 +0200

    Return old way of template files paths

--- lib/gotsha/config.rb
+++ lib/gotsha/config.rb
@@ -4,10 +4,10 @@ module Gotsha
   module Config
     CONFIG_DIR = ".gotsha"
     CONFIG_FILE = File.join(CONFIG_DIR, "config.yml")
-    CONFIG_TEMPLATE_PATH = "lib/gotsha/templates/config.yml"
+    CONFIG_TEMPLATE_PATH = File.expand_path("templates/config.yml", __dir__)
     GH_CONFIG_FILE = File.join(CONFIG_DIR, "github_action_example.yml")
-    GH_CONFIG_TEMPLATE_PATH = "lib/gotsha/templates/github_action_example.yml"
-    HOOKS_TEMPLATES_DIR = "lib/gotsha/templates"
+    GH_CONFIG_TEMPLATE_PATH = File.expand_path("templates/github_action_example.yml", __dir__)
+    HOOKS_TEMPLATES_DIR = File.expand_path("gotsha/templates", __dir__)
     HOOKS_DIR = File.join(CONFIG_DIR, "hooks")
   end
 end
