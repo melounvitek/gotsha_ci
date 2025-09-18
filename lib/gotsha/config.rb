ee3a232f37eaa249634212ae6904b5cca805e14c
Vitek <vitek@meloun.info>
2025-09-18 23:58:47 +0200

    Another fix

--- lib/gotsha/config.rb
+++ lib/gotsha/config.rb
@@ -7,7 +7,7 @@ module Gotsha
     CONFIG_TEMPLATE_PATH = File.expand_path("templates/config.yml", __dir__)
     GH_CONFIG_FILE = File.join(CONFIG_DIR, "github_action_example.yml")
     GH_CONFIG_TEMPLATE_PATH = File.expand_path("templates/github_action_example.yml", __dir__)
-    HOOKS_TEMPLATES_DIR = File.expand_path("gotsha/templates", __dir__)
+    HOOKS_TEMPLATES_DIR = File.expand_path("templates", __dir__)
     HOOKS_DIR = File.join(CONFIG_DIR, "hooks")
   end
 end
