553506f964af98d53c69583c02e7fd519903cdab
Vítek Meloun <vitek@meloun.info>
2025-09-19 23:55:39 +0200

    Rename

--- lib/gotsha/config.rb
+++ lib/gotsha/config.rb
@@ -5,7 +5,7 @@ module Gotsha
     CONFIG_DIR = ".gotsha"
     CONFIG_FILE = File.join(CONFIG_DIR, "config.yml")
     CONFIG_TEMPLATE_PATH = File.expand_path("templates/config.yml", __dir__)
-    GH_CONFIG_FILE = File.join(CONFIG_DIR, "github_action_example.yml")
+    GH_CONFIG_FILE = File.join(CONFIG_DIR, "github_action.yml")
     GH_CONFIG_TEMPLATE_PATH = File.expand_path("templates/github_action_example.yml", __dir__)
     HOOKS_TEMPLATES_DIR = File.expand_path("templates", __dir__)
     HOOKS_DIR = File.join(CONFIG_DIR, "hooks")
