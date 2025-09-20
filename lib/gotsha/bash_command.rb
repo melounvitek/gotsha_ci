58dd67e34b84160a53bd6e4b1d4c5c9007fba09d
Vitek <vitek@meloun.info>
2025-09-20 15:39:42 +0200

    Rename

--- lib/gotsha/bash_command.rb
+++ lib/gotsha/bash_command.rb
@@ -10,7 +10,7 @@ module Gotsha
 
       IO.popen(command) do |io|
         io.each do |line|
-          Config::USER_CONFIG["debug"] && puts(line)
+          Config::USER_CONFIG["verbose"] && puts(line)
 
           output << line
         end
@@ -33,7 +33,7 @@ module Gotsha
     end
 
     def self.silent_run!(command)
-      return run!(command) if Config::USER_CONFIG["debug"]
+      return run!(command) if Config::USER_CONFIG["verbose"]
 
       run!("#{command} > /dev/null 2>&1")
     end
