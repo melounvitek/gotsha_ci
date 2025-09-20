d0087b5324c3165370d24ee2233a3e152bf3e0f5
Vít Meloun <vitek@meloun.info>
2025-09-20 17:32:30 +0200

    Centralize all `require` and `require_relative` (#22)

--- lib/gotsha.rb
+++ lib/gotsha.rb
@@ -4,6 +4,10 @@ require "fileutils"
 require "yaml"
 
 require_relative "gotsha/action_dispatcher"
+require_relative "gotsha/actions/init"
+require_relative "gotsha/actions/run"
+require_relative "gotsha/actions/verify"
+require_relative "gotsha/bash_command"
 require_relative "gotsha/config"
 require_relative "gotsha/errors"
 require_relative "gotsha/version"
@@ -11,4 +15,6 @@ require_relative "gotsha/version"
 module Gotsha
   include Config
   include Errors
+
+  # Main entry-point: `Gotsha::ActionDispatcher.call(action_name)`
 end
