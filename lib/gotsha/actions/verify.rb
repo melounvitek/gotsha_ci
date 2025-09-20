da5fd256daecee659a47c2cc631874a109855f07
Vitek <vitek@meloun.info>
2025-09-20 18:39:24 +0200

    Fix Git haning forever

--- lib/gotsha/actions/verify.rb
+++ lib/gotsha/actions/verify.rb
@@ -4,8 +4,8 @@ module Gotsha
   module Actions
     class Verify
       def call
-        last_commit_sha = BashCommand.run!("git rev-parse HEAD").text_output.strip
-        last_comment_note = BashCommand.run!("git notes --ref=gotsha show #{last_commit_sha}").text_output.strip
+        last_commit_sha = BashCommand.run!("git --no-pager rev-parse HEAD").text_output.strip
+        last_comment_note = BashCommand.run!("git --no-pager notes --ref=gotsha show #{last_commit_sha}").text_output.strip
 
         raise(Errors::HardFail, "not verified yet") unless last_comment_note == "ok"
 
