e9a0e4ec681cc4ef02b329d520725d60c7a7e37b
Vitek <vitek@meloun.info>
2025-09-20 19:44:07 +0200

    Intentional mistake

--- lib/gotsha/actions/verify.rb
+++ lib/gotsha/actions/verify.rb
@@ -7,7 +7,7 @@ module Gotsha
         last_commit_sha = BashCommand.run!("git --no-pager rev-parse HEAD").text_output
 
         last_comment_note =
-          BashCommand.run!("git --no-pager notes --ref=gotsha show #{last_commit_sha}").text_output
+          BashCommand.run!("it --no-pager notes --ref=gotsha show #{last_commit_sha}").text_output
 
         raise(Errors::HardFail, "not verified yet") unless last_comment_note.length.positive?
         raise(Errors::HardFail, "tests failed") if last_comment_note.start_with?("Tests failed:")
